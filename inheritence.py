617a44633b52eb0509f860152b662de4322a3745
SATHISH9910 <sathishmungi@gmail.com>
2026-07-14 12:05:14 +0530

    Update inheritence.py

--- inheritence.py
+++ inheritence.py
@@ -1,15 +1,19 @@
 class Emoloyee():
     def working(self):
-        print("Emoloyee is working")
+        print("Emoloyee is working")                # in oops inheritence is where one class gets properties and methods of another class
     
-class Developer(Emoloyee):
+class Developer(Emoloyee):                       #child >- parent 
+                                                                                          
+                                                             # employee is parent class
+                                                #developer,tester is child class
     def coding(self):
-        print("developer is coding")
-
-class Tester(Emoloyee):
+        print("developer is coding")                                                    # this enables code reusability 
+                                                  # dev,test inherits() working( method) from employee
+    class Tester(Emoloyee):
     def testing(self):
         print("Tester is testing")
 
+                                             
 e=Emoloyee()
 e.working()
 
