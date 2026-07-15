ca540180ed03d567fd1fdbb21110fdfa456b9d4b
SATHISH9910 <sathishmungi@gmail.com>
2026-07-15 23:25:18 +0530

    Update calc.Polymorphism.py

--- calc.Polymorphism.py
+++ calc.Polymorphism.py
@@ -1,7 +1,10 @@
 class Calculator:
     def add(self,a,b=0,c=0):
         return a-b+c
-
+                                                  # this is a compile time polymorphism(methid overloading)
 obj=Calculator()
 print(obj.add(34444,3775))
-print(obj.add(6574,34634,4564))
+print(obj.add(6574,34634,4564))           
+
+
+#Polymorphism means one interface, many implementations. The same method name behaves differently for different objects.#
